f7e9cd7036d6dfa20e88102ef217fa960d30d3cc
letanque <frankie.martinez@gmail.com>
2023-02-16 04:06:40 -0800

    All dark

--- theme.ts
+++ theme.ts
@@ -2,7 +2,7 @@ import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
-  useSystemColorMode: true,
+  useSystemColorMode: false,
 };
 
 const theme = extendTheme({ config });
